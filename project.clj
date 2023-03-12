db2bd1856ce22e550ca850a94c6d97a5367b8966
Alfian Pratama <alfianpratama@hotmail.com>
2023-03-12 21:53:01 +0700

    init

--- project.clj
+++ project.clj
@@ -2,6 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns alfian-blog.core})
+            :url "https://www.eclipse.org/legal/epl-2.0/"} 
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core            "1.9.5"]
+                 [org.immutant/web          "2.1.10"]
+                 [rum                       "0.12.8"]
+                 [compojure                 "1.6.2"]
+                 [joda-time                 "2.10.14"]] 
+  :repl-options {:init-ns alfian-blog.core}
+  :main alfian_blog.server)
--- src/alfian_blog/core.clj
+++
@@ -1,6 +0,0 @@
-(ns alfian-blog.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
